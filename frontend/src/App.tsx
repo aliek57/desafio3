861f91d1ce2b1759d4509b473735587a08ef4d84
Keila <keilaprado@alunos.utfpr.edu.br>
2024-09-17 22:07:12 -0300

    feat: add responsive sign up page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login/Login"
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
 
   return (
-    <>
-      <div>
-        <Login/>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,14 +1,24 @@
 import { Link } from 'react-router-dom';
-import styles from './Login.module.css';
+import styles from './SignUp.module.css';
 import { FaGoogle, FaFacebook } from 'react-icons/fa';
 
-const Login = () => {
+const SignUp = () => {
   return (
     <div className='d-flex align-items-center justify-content-center vh-100'>
       <div className={styles.loginContainer}>
         <img src='/assets/logo_trisog.png' alt="logo" className={styles.loginLogo}/>
-        <h1 className={styles.loginTitle}>Sign In!</h1>
+        <h1 className={styles.loginTitle}>Sign Up!</h1>
         <form>
+          <div className='row'>
+            <div className='col-md-6 mb-3'>
+              <label htmlFor='firstName' className='form-label'>First Name:</label>
+              <input type='text' className='form-control' id='firstName' placeholder='First name'/>
+            </div>
+            <div className='col-md-6 mb-3'>
+              <label htmlFor='lastName' className='form-label'>Last Name:</label>
+              <input type='text' className='form-control' id='lastName' placeholder='Last name'/>
+            </div>
+          </div>
             <div className='mb-3'>
                 <label htmlFor='email' className='form-label'>E-mail:</label>
                 <input type='email' className='form-control' id='email' placeholder='Insert your email'/>
@@ -22,18 +32,18 @@ const Login = () => {
         <div className={styles.divider}><span>or</span></div>
         <button className={`${styles.googleButton} w-100 mb-2`}>
             <FaGoogle className='me-2' />
-            Sign in with Google
+            Sign up with Google
         </button>
         <button className={`${styles.facebookButton} w-100 mb-2`}>
             <FaFacebook className='me-2' />
-            Sign in with Facebook
+            Sign up with Facebook
         </button>
         <div className='text-center mt-3'>
-            Don't have an account yet? <Link to="/signup">Sign Up</Link>
+            Already have an account? <Link to="/login">Sign In</Link>
         </div>
       </div>
     </div>
   )
 }
 
-export default Login
+export default SignUp
